034569f5ea0b6a02559e324d7613e57afa11de59
dancosley <coslito@gmail.com>
2021-08-29 21:32:18 +0900

    Create README.md

--- Roman Numeral Chord Progression Generator.py
+++ Roman Numeral Chord Progression Generator.py
@@ -1,15 +1,15 @@
-#Next: Reconsider enharmonic spellings, ♭IV?, etc. Which to exclude?
+#Next: Reconsider enharmonic spellings, ♭IV?, #iii, etc. Which to exclude/include?
+#Remove duplicates in roman_nemrals!
+#Prevent duplicates in output
+
 
 import random
 roman_numeral = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii',
-                 'I⁺', 'II⁺', 'III⁺', 'IV⁺', 'V⁺', 'VI⁺', 'VII⁺', 'iᵒ', 'iiᵒ', 'iiiᵒ', 'ivᵒ', 'vᵒ', 'viᵒ', 'viiᵒ',
-                 'I', '♭II', '♭III', 'IV', '♭V', '♭VI', '♭VII', 'i', '♭ii', '♭iii', 'iv', '♭v', '♭vi', '♭vii',
-                 'I⁺', '♭II⁺', '♭III⁺', 'IV⁺', '♭V⁺', '♭VI⁺', '♭VII⁺', 'iᵒ', '♭iiᵒ', '♭iiiᵒ', 'ivᵒ', '♭vᵒ', '♭viᵒ',
-                 '♭viiᵒ'
-                 '♯I', '♯II', '♯III', '♯IV', '♯V', '♯VI', 'VII', '♯i', '♯ii', '♯iii', '♯iv', '♯v', '♯vi', 'vii',
-                 '♯I⁺', '♯II⁺', '♯III⁺', '♯IV⁺', '♯V⁺', '♯VI⁺', '♯VII⁺', '♯iᵒ', '♯iiᵒ', '♯iiiᵒ', '♯ivᵒ', '♯vᵒ', '♯viᵒ',
-                 'viiᵒ'
-                 )
+                 'I⁺', 'II⁺', 'III⁺', 'IV⁺', 'V⁺', 'VI⁺', 'VII⁺', 'iᵒ', 'iiᵒ', 'iiiᵒ', 'ivᵒ', 'vᵒ', 'viᵒ', 'viiᵒ', '♭II',
+                 '♭III', 'IV', '♭V', '♭VI', '♭VII', 'i', '♭ii', '♭iii', 'iv', '♭v', '♭vi', '♭vii',
+                 'I⁺', '♭II⁺', '♭III⁺', 'IV⁺', '♭V⁺', '♭VI⁺', '♭VII⁺', '♭iiᵒ', '♭iiiᵒ', '♭vᵒ', '♭viᵒ',
+                 '♭viiᵒ', '♯II', '♯IV', '♯V', '♯VI', 'VII', '♯ii', '♯iii', '♯iv', '♯v', '♯vi', '♯II⁺', '♯IV⁺',
+                 '♯V⁺', '♯VI⁺', '♯iiᵒ', '♯ivᵒ', '♯vᵒ', '♯viᵒ')
 tonics = ("A", "A♯", "B", "B♯", "C", "C♯", "D", "D♯", "E", "E♯", "F", "F♯", "G", "G♯", "A♭", "B♭", "C♭", "D♭", "E♭",
          "F♭", "G♭")
 #Generate!
