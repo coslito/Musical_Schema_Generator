b7390085d1df239e02c278461b433854ea27309f
dancosley <coslito@gmail.com>
2021-08-29 21:23:51 +0900

    Create README.md

--- Roman Numeral Chord Progression Generator.py
+++ Roman Numeral Chord Progression Generator.py
@@ -1,4 +1,4 @@
-#Next: Reconsider enharmonic spellings, ♭IV?, etc...
+#Next: Reconsider enharmonic spellings, ♭IV?, etc. Which to exclude?
 
 import random
 roman_numeral = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii',
@@ -10,8 +10,10 @@ roman_numeral = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'i', 'ii', 'iii', 'iv
                  '♯I⁺', '♯II⁺', '♯III⁺', '♯IV⁺', '♯V⁺', '♯VI⁺', '♯VII⁺', '♯iᵒ', '♯iiᵒ', '♯iiiᵒ', '♯ivᵒ', '♯vᵒ', '♯viᵒ',
                  'viiᵒ'
                  )
-
+tonics = ("A", "A♯", "B", "B♯", "C", "C♯", "D", "D♯", "E", "E♯", "F", "F♯", "G", "G♯", "A♭", "B♭", "C♭", "D♭", "E♭",
+         "F♭", "G♭")
 #Generate!
+print("\nTONIC:")
+print(random.choice(tonics))
 print("\nCHORD PROGRESSION:")
-print(random.choice(roman_numeral), "    ", random.choice(roman_numeral),
-"    ", random.choice(roman_numeral), "    ", random.choice(roman_numeral))
+print(random.choice(roman_numeral), "    ", random.choice(roman_numeral), "    ", random.choice(roman_numeral), "    ", random.choice(roman_numeral))
