17ddf3c2b95120c6e489263542dc42048122cf1d
dancosley <coslito@gmail.com>
2021-09-01 16:13:41 +0900

    Create README.md

--- Parent Scale Transitions Generator.py
+++ Parent Scale Transitions Generator.py
@@ -4,7 +4,8 @@ import random
 roots = ("A", "A♯", "B", "B♯", "C", "C♯", "D", "D♯", "E", "E♯", "F", "F♯", "G", "G♯", "A♭", "B♭", "C♭", "D♭", "E♭",
          "F♭", "G♭")
 parent_scales = ('Major', 'Melodic Minor', 'Harmonic Minor', 'Half-Whole Diminished', 'Whole-Tone', 'Minor Pentatonic',
-                 'Minor Blues', 'Lydian', 'Dorian', 'Mixolydian', 'Altered')
+                 'Minor Blues', 'Lydian', 'Dorian', 'Mixolydian', 'Altered', 'Minor Pentatonic add 2',
+                 'Minor Pentatonic add 6')
 
 caged_position = ("C", "A", "G", "E", "D")
 
